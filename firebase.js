f4502fb2321be7a36a941c4ae56b8e83c590a422
Stoil100 <toli100ilov@gmail.com>
2023-10-21 14:10:50 +0300

    initialized project

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth }from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnbjTMlBkbeD15djiMJXfqaB89bqRb2Qk",
+  authDomain: "smart-feeder-backend.firebaseapp.com",
+  projectId: "smart-feeder-backend",
+  storageBucket: "smart-feeder-backend.appspot.com",
+  messagingSenderId: "359818048994",
+  appId: "1:359818048994:web:84d389f66338af1055f5bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth=getAuth(app);
+const firestore = getFirestore(app);
+
+export {auth, firestore};
