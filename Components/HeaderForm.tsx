6d22c80b2910ac0559a39a6ff63a1e08ed6534c0
Stoil100 <toli100ilov@gmail.com>
2023-10-24 22:05:29 +0300

    Added header for Signup and Login pages

---
+++ Components/HeaderForm.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+
+interface Props{
+    title: string
+}
+
+const HeaderForm = ({title}:Props) => {
+  return (
+    <View style={styles.container}>
+       <Image source={require('./img/smartFeederLogo.png')} style={{width: 100, height: 100}} />
+      <Text style={styles.headerTitle}>Welcome to Smart Feeder</Text>
+      <Text style={styles.headerText}>{title}</Text>
+    </View>
+  )
+}
+
+export default HeaderForm
+
+const styles = StyleSheet.create({
+
+    container:{
+       justifyContent: 'center',
+       alignItems: 'center',
+    },
+    header:{
+        fontWeight: "700",
+        fontSize: 32,
+    },
+        headerTitle:{
+        fontWeight: "700",
+        fontSize: 24,
+    },
+    headerText:{
+        fontSize: 20,
+    }
+})
